f0c11b2f6ce3962c32c0a89debdbf5c8d7ddf722
Igaki12 <91039412+Igaki12@users.noreply.github.com>
2022-09-12 15:47:31 +0900

    added questions

--- src/useQuestionList.js
+++ src/useQuestionList.js
@@ -6,17 +6,37 @@ export const useQuestionList = () => {
   const [questionList, setQuestionList] = useState([
     // ここに問題のリストを記述(選択式の場合は[0]に正解択をいれた配列をつくる)
     {
-      groupTag: '2021',
+      groupTag: '2020',
       groupContents: [
         {
           detailInfo: '',
           questionImg: [],
-          questionSentence: 'てすと',
+          questionSentence:
+            '一次運動野において、特に大きなものをベッツ細胞と呼ぶこともある出力細胞の名称を答えよ。',
           choices: [],
           answerImg: [],
           answer: 'てすと',
           commentary: 'てすと',
         },
+        // {
+        //   detailInfo: '',
+        //   questionImg: [],
+        //   questionSentence: '',
+        //   choices: [],
+        //   answerImg: [],
+        //   answer: '',
+        //   commentary: '',
+        // },
+        // {
+        //   detailInfo: '',
+        //   questionImg: [],
+        //   questionSentence:
+        //     '',
+        //   choices: [],
+        //   answerImg: [],
+        //   answer: '',
+        //   commentary: '',
+        // },
       ],
     },
   ])
